3a443a3b12d9fc5028c9d27ec711049e8efb3208
Cameron Tacklind <cameron@tacklind.com>
2014-12-17 16:13:42 -0800

    Cleanup

--- MLX90363.cpp
+++ MLX90363.cpp
@@ -29,15 +29,6 @@ void MLX90363::init() {
  SS.output();
  SR->byte = 0;
  CR->byte = 0b11010100;
- 
- buffer[0] =
- buffer[1] =
- buffer[2] =
- buffer[3] =
- buffer[4] =
- buffer[5] =
- buffer[6] =
- buffer[7] = 0;
 }
 
 void MLX90363::startTransmitting() {
