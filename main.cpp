1225840663d5a1e6f7a6130b502b022481bfeb5e
Cameron Tacklind <cameron@tacklind.com>
2014-12-17 16:54:11 -0800

    Better init

--- main.cpp
+++ main.cpp
@@ -12,19 +12,26 @@
 #include "ThreePhaseDriver.h"
 #include "MLX90363.h"
 
+/**
+ * All the init functions should go in here.
+ * gcc will automatically call it for us because of the constructor attribute.
+ */
+void init() __attribute__((constructor));
+
+void init() {
+ Board::LED.output();
+
+ // Make sure the ^SS pin is a driven output BEFORE initializing the SPI hardware.
+ // The AVR's ^SS pin is really Board::DRV::AL, controlled by ThreePhaseDriver.
+ ThreePhaseDriver::init();
+ MLX90363::init();
+}
+
 /*
  * 
  */
 void main() {
- 
- 
-// Board::LED.output();
  Board::LED.on();
- 
- /** Make sure the ^SS pin is a driven output BEFORE initializing the SPI hardware.
-  */
- ThreePhaseDriver::init();
- MLX90363::init();
 
  while(1);
 }
