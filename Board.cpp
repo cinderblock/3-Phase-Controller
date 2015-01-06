9e19b99ee85b790d09fe8c1e4dbe38741dee54f5
Cameron Tacklind <cameron@tacklind.com>
2015-01-05 17:39:11 -0800

    Comment cleanup

--- Board.cpp
+++ Board.cpp
@@ -6,29 +6,27 @@
  */
 
 #include "Board.h"
-
-//From tool collection winAVR
 #include <avr/io.h>
 
-//LED - indicator
+// LED - indicator
 IOpin Board::LED        = IOpin(&PORTD, 4);
 
-//I2C
+// I2C
 IOpin Board::I2C::SCL        = IOpin(&PORTD, 0);
 IOpin Board::I2C::SDA        = IOpin(&PORTD, 1);
 
-//SPI
+// SPI
 IOpin Board::SPI::SCLK       = IOpin(&PORTB, 1);
 IOpin Board::SPI::MOSI       = IOpin(&PORTB, 2);
 IOpin Board::SPI::MISO       = IOpin(&PORTB, 3);
 IOpin Board::MagSel          = IOpin(&PORTD, 5);
 
-//Input Voltage Checks
+// Input Voltage Checks
 IOpin Board::AIN0       = IOpin(&PORTE, 6);
 IOpin Board::VBATS      = IOpin(&PORTF, 5);
 u1    Board::MUX::VBATS = 0b000101; // ADC5
 
-//FET control lines
+// FET control lines
 IOpin Board::DRV::AH         = IOpin(&PORTB, 5);
 IOpin Board::DRV::AL         = IOpin(&PORTB, 4);
 IOpin Board::DRV::BH         = IOpin(&PORTB, 6);
@@ -46,6 +44,6 @@ u1    Board::MUX::AS = 0b100010; // ADC10
 u1    Board::MUX::BS = 0b000100; // ADC4
 u1    Board::MUX::CS = 0b100001; // ADC9
 
-//Serial
+// Serial
 IOpin Board::SER::Rx         = IOpin(&PORTD, 2);
 IOpin Board::SER::Tx         = IOpin(&PORTD, 3);
