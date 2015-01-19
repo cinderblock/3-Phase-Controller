7f9f45ec5c7b460c593b05cfd4587196e314b3a1
Cameron Tacklind <cameron@tacklind.com>
2015-01-18 21:27:43 -0800

    Higher maximum

--- ThreePhaseDriver.h
+++ ThreePhaseDriver.h
@@ -69,7 +69,7 @@ public:
   */
  static void advanceToFullSine(Phase const phase, u1 const step);
  
- static constexpr u1 maxAmplitude = 220;
+ static constexpr u1 maxAmplitude = 0xff - 30;
  
  static inline void setAmplitude(u1 const a) {amplitude = a > maxAmplitude ? maxAmplitude : a;}
 
