0d461a72e869480e1a34cb0e122b0966842559ed
Cameron Tacklind <cameron@tacklind.com>
2015-02-08 23:50:07 -0800

    Developing the I2C interface software. Should basically work. Needs data setters and getters and enablers, but w/e.

--- TwillBotInterface.cpp
+++ TwillBotInterface.cpp
@@ -10,8 +10,91 @@
 
 using namespace AVR::I2C;
 
-void TwillBotInterface::init() {
- CR->byte = 0b01000101;
+u1 TwillBotInterface::incomingBuffer[];
+u1 TwillBotInterface::outgoingBuffer[];
 
- *(ARt*const)AR = {true,address};
+u1 TwillBotInterface::incomingIndex;
+u1 TwillBotInterface::outgoingIndex;
+
+bool TwillBotInterface::generalCall;
+
+void TWI_vect() {
+ TwillBotInterface::isr();
 }
+
+void TwillBotInterface::init() {
+ outgoingIndex = 0;
+ incomingIndex = 0;
+
+ *(ARt*const)AR = {generalCallEnable,address};
+ *AMR = 0;
+ 
+ CR->byte = 0b01000101;
+}
+
+void TwillBotInterface::isr() {
+ Status s = AVR::I2C::getStatus();
+ bool nack = false;
+ 
+ if (s == Status::SL_SLA_W_ACK) {
+  // Don't need to do anything. Everything should be ready for receiving data
+  generalCall = false;
+ }
+ if (s == Status::SL_SLA_W_ACK_ArbitrationLost) {
+  // This would only happen if we were a master while trying to send and then
+  // were told to be a slave
+ }
+ if (s == Status::SL_General_ACK) {
+  generalCall = true;
+  // Return nack for now
+  nack = true;
+ }
+ if (s == Status::SL_General_ACK_ArbitrationLost) {
+  // This would only happen if we were a master while trying to send and then
+  // some other master tried to go to the general call
+  generalCall = false;
+ }
+ if (s == Status::SL_DATA_RX_ACK) {
+  incomingBuffer[incomingIndex++] = *DR;
+  if (incomingIndex == incomingLength)
+   nack = true;
+ }
+ if (s == Status::SL_DATA_RX_NACK) {
+  incomingBuffer[incomingIndex] = *DR;
+ }
+ if (s == Status::SL_General_DATA_RX_ACK) {
+  nack = true;
+  // TODO: handle general call
+ }
+ if (s == Status::SL_General_DATA_RX_NACK) {
+  // TODO: handle general call
+ }
+ if (s == Status::SL_STOP_RESTART) {
+  
+ }
+ 
+ if (s == Status::SL_SLA_R_ACK) {
+  *DR = outgoingBuffer[outgoingIndex++];
+  if (outgoingIndex == outgoingLength)
+   nack = true;
+ }
+ if (s == Status::SL_SLA_R_ACK_ArbitrationLost) {
+  // Not a master. Won't get here.
+ }
+ if (s == Status::SL_DATA_TX_ACK) {
+  *DR = outgoingBuffer[outgoingIndex++];
+  if (outgoingIndex == outgoingLength)
+   nack = true;
+ }
+ if (s == Status::SL_DATA_TX_NACK) {
+  *DR = outgoingBuffer[outgoingIndex];
+ }
+ if (s == Status::SL_DATA_TX_ACK_Done) {
+  nack = true;
+ }
+ 
+// CR->EnableAcknowledge = nack;
+// CR->InterruptFlag = 1;
+ CR->byte = nack ? 0b10000101 : 0b11000101;
+}
+
--- TwillBotInterface.h
+++ TwillBotInterface.h
@@ -15,8 +15,23 @@ ISR(TWI_vect);
 
 class TwillBotInterface {
  static constexpr b7 address = Config::I2CslaveAddress;
+ static constexpr bool generalCallEnable = true;
  friend void TWI_vect();
+ inline static void isr();
  
+ static constexpr u1 incomingBufferSize = 10;
+ static constexpr u1 outgoingBufferSize = 10;
+ 
+ static constexpr u1 incomingLength = incomingBufferSize;
+ static constexpr u1 outgoingLength = outgoingBufferSize;
+ 
+ static u1 incomingIndex;
+ static u1 outgoingIndex;
+ 
+ static bool generalCall;
+ 
+ static u1 incomingBuffer[incomingBufferSize];
+ static u1 outgoingBuffer[outgoingBufferSize];
 public:
  
  static void init();
