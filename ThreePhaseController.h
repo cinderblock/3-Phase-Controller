c2370f94545b3b965acfd29900dac0e8706f1fce
Cameron Tacklind <cameron@tacklind.com>
2015-10-22 02:36:14 -0700

    Interface thoughts

--- ThreePhaseController.h
+++ ThreePhaseController.h
@@ -8,10 +8,19 @@
 #ifndef THREEPHASECONTROLLER_H
 #define	THREEPHASECONTROLLER_H
 
+#include <AVR++/basicTypes.h>
+
 class ThreePhaseController {
+ static s2 position;
+ static s2 velocity;
+ 
 public:
  static void init();
-private:
+ 
+ static void setTorque(s2 t);
+ 
+ inline static u2 getPosition() {return position;}
+ inline static s2 getVelocity() {return velocity;}
 
 };
 
