e61dcea6d7decca1b5b73a0a04a356322d7d5ceb
Cameron Tacklind <cameron@tacklind.com>
2015-10-26 16:33:40 -0700

    Use some dead time

--- ThreePhaseDriver.cpp
+++ ThreePhaseDriver.cpp
@@ -50,7 +50,7 @@ void ThreePhaseDriver::init() {
   * DT4H3 DT4H2 DT4H1 DT4H0 DT4L3 DT4L2 DT4L1 DT4L0
   * 0b  0     0     0     0     0     0     0     0
   */
- DT4 = 0;
+ DT4 = 0xff;
 
  /**
   * TCCR4D
