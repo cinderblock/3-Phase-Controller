f0e9e7dffb35296d324cbd8783914c9c7553894f
Cameron Tacklind <cameron@tacklind.com>
2015-10-26 23:44:31 -0700

    Calculate output size too

--- local.mk
+++ local.mk
@@ -2,3 +2,5 @@
 #DFU = "C:/Program Files/WinAVR/bin/dfu-programmer.exe"
 BLD_BIN_PREFIX = C:/Program Files (x86)/avr-gcc-4.9.2_2014-09-12_mingw32/bin/
 BLD_SZE = "C:/Program Files/WinAVR/bin/avr-size" --format=avr --mcu=$(MCU)
+
+all: size
