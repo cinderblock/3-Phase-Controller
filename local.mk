9d3007b364184cc21e5c19059965c8823b4baefc
Cameron Tacklind <cameron@tacklind.com>
2016-04-02 02:46:37 -0700

    Add support for hostname based local config files

--- local.mk
+++ local.mk
@@ -3,4 +3,6 @@
 # BLD_BIN_PREFIX = C:/Program Files (x86)/avr-gcc-4.9.2_2014-09-12_mingw32/bin/
 # BLD_SZE = "C:/Program Files/WinAVR/bin/avr-size" --format=avr --mcu=$(MCU)
 
+-include local.$(shell hostname).mk
+
 all: size
