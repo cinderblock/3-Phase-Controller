92ffc8d92fb0c8a70c1ad0e142c939e207dbd0c8
Blake Tacklind <blake@tacklind.com>
2016-06-14 14:56:49 -0700

    general structure flushed out

--- ServoController.cpp
+++ ServoController.cpp
@@ -1,9 +1,71 @@
 
 #include "ServoController.h"
 
+ServoController::Mode ServoController::currentMode;
+
+s2 ServoController::torqueCommand;
+s2 ServoController::velocityCommand;
+s4 ServoController::positionCommand;
+
+ServoController::P;
+ServoController::I;
+ServoController::D;
+
+u1 ServoController::currentLimit;
+
 void ServoController::init(){
+	currentMode = Mode::INIT;
+
+
+}
+
+void ServoController::update(){
 	
 }
 
 
+void ServoController::setTorque(s2 torque){
+	currentMode = Mode::TOR;
+
+	torqueCommand = torque;
+}
+
+void ServoController::setVelocity(s2 velocity){
+	currentMode = Mode::VEL;
+
+	velocityCommand = velocity;
+}
+
+void ServoController::setPosition(s4 position){
+	currentMode = Mode::POS;
+
+	positionCommand = position;
+}
+
+
+void ServoController::setCurrentLimit(u1 current){
+	currentLimit = current;
+
+}
+
+
+void ServoController::setP(u1 p){
+	P = p;
+}
+
+void ServoController::setI(u1 i){
+	I = i;
+}
+
+void ServoController::setD(u1 d){
+	D = d;
+}
+
+
+void ServoController::setTorqueEnable(bool enable){
+
+	if(!enable)
+		currentMode = Mode::INIT;
+
+}
 
--- ServoController.h
+++ ServoController.h
@@ -4,23 +4,40 @@
 
 class ServoController {
 private:
-	
+
+	enum class Mode:u1 {INIT, TOR, VEL, POS};
+
+	static Mode currentMode;
+
+	static s2 torqueCommand;
+	static s2 velocityCommand;
+	static s4 positionCommand;
+
+	static u1 P;
+	static u1 I;
+	static u1 D;
+
+	static u1 currentLimit;
 
 public:
 	static void init();
 
+	static void update();
+
 	static void setTorque(s2);
 	static void setVelocity(s2);
+
+	static void setZero();
 	static void setPosition(s4);
 
-	static void setCurrent(u1);
+	static void setCurrentLimit(u1);
 
 	static void setP(u1);
 	static void setI(u1);
 	static void setD(u1);
 
-	static void setTorqueEnable(u1);
+	static void setTorqueEnable(bool);
 
-	
+
 };
 
