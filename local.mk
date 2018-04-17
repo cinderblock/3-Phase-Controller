130162bd80521824c77f494090c3576a84161dd0
Cameron Tacklind <cameron@tacklind.com>
2018-04-16 19:28:10 -0700

    Remove local.mk

--- local.mk
+++
@@ -1,8 +0,0 @@
-
-#DFU = "C:/Program Files/WinAVR/bin/dfu-programmer.exe"
-# BLD_BIN_PREFIX = C:/Program Files (x86)/avr-gcc-4.9.2_2014-09-12_mingw32/bin/
-# BLD_SZE = "C:/Program Files/WinAVR/bin/avr-size" --format=avr --mcu=$(MCU)
-
--include local.$(shell hostname).mk
-
-all: size
