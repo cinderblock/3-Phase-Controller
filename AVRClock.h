93e9528f29e9ac328c1a46ace01e9afce1681c44
Cameron Tacklind <cameron@tacklind.com>
2018-04-19 19:07:29 -0700

    Use enum classes for options to enabling PLL and add more ways of enabling

--- AVRClock.h
+++ AVRClock.h
@@ -14,10 +14,55 @@
 
 namespace AVR {
  namespace Clock {
-  inline void enablePLL(u1 const PLL = 10, u1 const timerDiv = 2, bool const useEXT = true) {
-   PLLFRQ = ((useEXT ? 0 : 1) << PINMUX) | (timerDiv << PLLTM0) | (1 << PLLUSB) | PLL;
-   bool const divideClock = useEXT && (F_CPU == 16000000UL);
-   PLLCSR = ((divideClock ? 1 : 0) << PINDIV) | (1 << PLLE);
+   enum class PLLOutFreq : u1 {
+     f40MHz = 0b0011,
+     f48MHz = 0b0100,
+     f56MHz = 0b0101,
+     f72MHz = 0b0111,
+     f80MHz = 0b1000,
+     f88MHz = 0b1001,
+     f96MHz = 0b1010,
+   };
+   
+   enum class Timer4Div : u1 {
+     Disabled = 0b00,
+     One = 0b01,
+     OneAndHalf = 0b10,
+     Two = 0b11,
+   };
+   
+   static constexpr Timer4Div minTimerDivForPLL(PLLOutFreq const out) {
+     return (out >= PLLOutFreq::f72MHz ? Timer4Div::OneAndHalf : Timer4Div::One);
+   }
+   
+   static constexpr auto defaultPLLOutFreq = PLLOutFreq::f96MHz;
+   static constexpr bool defaultUseExternalCystal = true;
+   
+  inline void enablePLL(
+    bool const useEXT,
+    Timer4Div const timerDiv,
+    PLLOutFreq const PLL
+  ) {
+   PLLFRQ = ((useEXT ? 0 : 1) << PINMUX) | (u1(timerDiv) << PLLTM0) | ((PLL == PLLOutFreq::f96MHz ? 1 : 0) << PLLUSB) | u1(PLL);
+   
+   bool const dividePLLInputClock = useEXT && (F_CPU == 16000000UL);
+   
+   PLLCSR = ((dividePLLInputClock ? 1 : 0) << PINDIV) | (1 << PLLE);
+  }
+  
+  inline void enablePLL(
+    bool const useEXT,
+    PLLOutFreq const PLL = defaultPLLOutFreq
+  ) {
+    enablePLL(useEXT, minTimerDivForPLL(PLL), PLL);
+  }
+  
+  inline void enablePLL(PLLOutFreq const PLL = defaultPLLOutFreq) {
+    enablePLL(defaultUseExternalCystal, minTimerDivForPLL(PLL), PLL);
+  }
+  
+  inline void enablePLL(Timer4Div const timerDiv, PLLOutFreq const PLL) {
+    enablePLL(defaultUseExternalCystal, timerDiv, PLL);
   }
 
   inline void waitForPLL() {
@@ -27,4 +72,3 @@ namespace AVR {
 };
 
 #endif	/* AVRCLOCK_H */
-
