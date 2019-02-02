72b77abeb98c9ca28e7970ae6390c61c252d0460
Cameron Tacklind <cameron@tacklind.com>
2019-02-01 18:57:17 -0800

    Testing new ways to have constexpr pointers in modern C++

--- test.cpp
+++ test.cpp
@@ -1,5 +1,15 @@
-#include <util/delay.h>
+#include <avr/io.h>
+#include <stdint.h>
+constexpr volatile uint8_t *const PortA = &PORTB;
+int main() { *PortA |= 1; }
 
-void foobar() {
-  _delay_ms(1);
-}
+// constexpr auto PortA = &PORTB;
+
+// int main() { *PortA |= 1; }
+
+constexpr uintptr_t PA = 0x25;
+
+void set() { (*(volatile uint8_t *)(PA)) |= 1; }
+
+const char str[] = "FooBar";
+constexpr const char *x = str + 2;
