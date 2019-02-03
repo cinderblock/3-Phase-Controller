8d2cd3d14e085826569a5c5e259468be34cd1abb
Cameron Tacklind <cameron@tacklind.com>
2019-02-02 22:53:31 -0800

    Maybe references are the solution

--- test.cpp
+++ test.cpp
@@ -1,15 +1,21 @@
 #include <avr/io.h>
 #include <stdint.h>
-constexpr volatile uint8_t *const PortA = &PORTB;
-int main() { *PortA |= 1; }
+
+auto &PortA = PORTB;
+
+// constexpr volatile uint8_t *const PortA = &PORTB;
+int main() {
+  PortA |= 1;
+  PORTB |= 1;
+}
 
 // constexpr auto PortA = &PORTB;
 
 // int main() { *PortA |= 1; }
 
-constexpr uintptr_t PA = 0x25;
+// constexpr uintptr_t PA = 0x25;
 
-void set() { (*(volatile uint8_t *)(PA)) |= 1; }
+// void set() { (*(volatile uint8_t *)(PA)) |= 1; }
 
-const char str[] = "FooBar";
-constexpr const char *x = str + 2;
+// const char str[] = "FooBar";
+// constexpr const char *x = str + 2;
