7248383d75f2704c317dbf2c15a99de768ea5f96
Cameron Tacklind <cameron@tacklind.com>
2019-02-10 12:54:51 -0800

    Fix warnings

--- src/ConfigCheck.h
+++ src/ConfigCheck.h
@@ -2,13 +2,22 @@
 
 #define CONFIG_COUNT 0
 #ifdef BED_CONTROLLER
-#define CONFIG_COUNT CONFIG_COUNT + 1
+#define TEMP CONFIG_COUNT
+#undef CONFIG_COUNT
+#define CONFIG_COUNT TEMP + 1
+#undef TEMP
 #endif
 #ifdef QUANTUM_DRIVE
-#define CONFIG_COUNT CONFIG_COUNT + 1
+#define TEMP CONFIG_COUNT
+#undef CONFIG_COUNT
+#define CONFIG_COUNT TEMP + 1
+#undef TEMP
 #endif
 #ifdef HOVER_DRIVE
-#define CONFIG_COUNT CONFIG_COUNT + 1
+#define TEMP CONFIG_COUNT
+#undef CONFIG_COUNT
+#define CONFIG_COUNT TEMP + 1
+#undef TEMP
 #endif
 #if CONFIG_COUNT > 1
 #error "Too many configurations selected"
