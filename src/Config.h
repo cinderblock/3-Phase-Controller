52fb52cd0e9332e89946589ff70bb2c88efe4246
Cameron Tacklind <cameron@tacklind.com>
2019-02-27 22:54:14 -0800

    Remove old options

--- src/Config.h
+++ src/Config.h
@@ -8,11 +8,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// #define QUANTUM_DRIVE 1 // Sterring motor
-// #define QUANTUM_DRIVE 2 // traction motor
-// #define QUANTUM_DRIVE 3 // alternate traction motor
-//#define BED_CONTROLLER
-
+// #define QUANTUM_DRIVE
+// #define BED_CONTROLLER
 #define HOVER_DRIVE
 
 #include "ConfigCheck.h"
