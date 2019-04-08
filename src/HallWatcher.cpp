196e06306832517eb77468daed3a5766464fa2a8
Cameron Tacklind <cameron@tacklind.com>
2019-04-08 16:58:37 -0700

    Cleanup

--- src/HallWatcher.cpp
+++ src/HallWatcher.cpp
@@ -19,50 +19,32 @@ using namespace ThreePhaseControllerNamespace;
 u1 volatile HallWatcher::state = 0b111;
 void (*HallWatcher::stateChangeReceiver)() = nullptr;
 
-#ifdef BED_CONTROLLER
-void INT6_vect() { HallWatcher::checkH1(); }
-
-void PCINT0_vect() {
-  HallWatcher::checkH2();
-  HallWatcher::checkH3();
-}
+#ifdef QUANTUM_DRIVE
+void INT0_vect() { HallWatcher::checkH1(); }
+void INT1_vect() { HallWatcher::checkH2(); }
+void PCINT0_vect() { HallWatcher::checkH3(); }
 
 void HallWatcher::init() {
-  // set edge detecction INT6
-  EICRB = 0b00010000;
-  // Enable INT6
-  EIMSK = 0b01000000;
+  // set edge detection INT0 & INT1
+  EICRA = 0b00000101;
+  // Enable INT0 & INT1
+  EIMSK = 0b00000011;
 
-  // Enable PCINT7 & PCINT4
-  PCMSK0 = 0b10010000;
+  // Enable PCINT4
+  PCMSK0 = 0b00010000;
 #endif
 
-#ifdef QUANTUM_DRIVE
-  void INT0_vect() { HallWatcher::checkH1(); }
-  void INT1_vect() { HallWatcher::checkH2(); }
-  void PCINT0_vect() { HallWatcher::checkH3(); }
-
+#if defined(HOVER_DRIVE) || defined(BED_CONTROLLER)
   void HallWatcher::init() {
-    // set edge detection INT0 & INT1
-    EICRA = 0b00000101;
-    // Enable INT0 & INT1
-    EIMSK = 0b00000011;
-
-    // Enable PCINT4
-    PCMSK0 = 0b00010000;
+    return;
 #endif
 
-#ifdef HOVER_DRIVE
-    void HallWatcher::init() {
-      return;
-#endif
+    // TODO: Make the above ifdef/endif less hacky
 
-      // TODO: Make the above ifdef/endif less hacky
+    // Enable pin change interrupts in general
+    PCICR = 0b00000001;
 
-      // Enable pin change interrupts in general
-      PCICR = 0b00000001;
-
-      checkH1();
-      checkH2();
-      checkH3();
-    }
+    checkH1();
+    checkH2();
+    checkH3();
+  }
