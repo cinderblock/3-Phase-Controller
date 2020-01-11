a145ab0cbe8dba83f47752180aa4105bf4309840
Cameron Tacklind <cameron@tacklind.com>
2020-01-11 00:06:41 -0800

    Improve packet format definitions

--- src/USB/PacketFormats.h
+++ src/USB/PacketFormats.h
@@ -33,87 +33,86 @@ enum class CommandMode : u1 {
   Bootloader = 0xfe,
 };
 
-/**
- * Shape of data going OUT of host.
- */
+typedef struct {
+  u1 mlxData[8];
+  // Should CRC be calculated locally
+  bool crc;
+} MLXCommand;
+
+typedef struct {
+  u2 A;
+  u2 B;
+  u2 C;
+} ThreePhaseCommand;
+
+typedef struct {
+  u2 angle;
+  u1 amplitude;
+} CalibrationCommand;
+
+typedef struct {
+  s2 command;
+} PushCommand;
+
+typedef struct {
+  u1 mode;
+  s4 command;
+} ServoCommand;
+
+typedef struct {
+  u1 amplitude;
+  s4 velocity;
+} SynchronousCommand;
+
 typedef struct {
   CommandMode mode;
   union {
-
-    // MLXCommand
-    struct {
-      u1 mlxData[8];
-      // Should CRC be calculated locally
-      bool crc;
-    } mlx;
-
-    // ThreePhaseCommand
-    struct {
-      u2 A;
-      u2 B;
-      u2 C;
-    } ThreePhase;
-
-    // CalibrationCommand
-    struct {
-      u2 angle;
-      u1 amplitude;
-    } calibrate;
-
-    // PushCommand
-    struct {
-      s2 command;
-    } push;
-
-    // ServoCommand
-    struct {
-      u1 mode;
-      s4 command;
-    } servo;
-
-    // SynchronousCommand
-    struct {
-      u1 amplitude;
-      s4 velocity;
-    } synchronous;
+    MLXCommand mlx;
+    ThreePhaseCommand ThreePhase;
+    CalibrationCommand calibrate;
+    PushCommand push;
+    ServoCommand servo;
+    SynchronousCommand synchronous;
   };
-} USBDataOUTShape;
+} CommandFormat;
 
 /**
- * Shape of data going IN to host
+ * Shape of data going OUT of host.
  */
+typedef CommandFormat USBDataOUTShape;
+
+typedef struct {
+  Fault fault;
+} FaultData;
+
+typedef struct {
+  ThreePhaseDriver::PhasePosition position;
+  s4 velocity;
+  ThreePhaseController::Amplitude amplitude;
+
+  bool mlxDataValid;
+
+  u1 mlxResponse[8];
+  MLX90363::ResponseState mlxResponseState;
+} ManualData;
+
+typedef struct {
+  ThreePhaseDriver::PhasePosition position;
+  s2 velocity;
+  ThreePhaseController::Amplitude amplitude;
+
+  bool lookupValid : 1;
+
+  u2 controlLoops;
+  u2 mlxFailedCRCs;
+} NormalData;
+
 typedef struct {
   State state;
-
   union {
-    // Iff state == State::Fault
-    struct {
-      Fault fault;
-    } fault;
-
-    // Iff state == State::Manual
-    struct {
-      ThreePhaseDriver::PhasePosition position;
-      s4 velocity;
-      ThreePhaseController::Amplitude amplitude;
-
-      bool mlxDataValid;
-
-      u1 mlxResponse[8];
-      MLX90363::ResponseState mlxResponseState;
-    } manual;
-
-    // Iff state == State::Normal
-    struct {
-      ThreePhaseDriver::PhasePosition position;
-      s2 velocity;
-      ThreePhaseController::Amplitude amplitude;
-
-      bool lookupValid : 1;
-
-      u2 controlLoops;
-      u2 mlxFailedCRCs;
-    } normal;
+    FaultData fault;
+    ManualData manual;
+    NormalData normal;
   };
 
   // Analog group
@@ -128,7 +127,12 @@ typedef struct {
     u2 BS;
     u2 CS;
   };
-} USBDataINShape;
+} DataFormat;
+
+/**
+ * Shape of data going IN to host
+ */
+typedef DataFormat USBDataINShape;
 
 static_assert(sizeof(USBDataOUTShape) <= REPORT_SIZE, "Data going OUT of HOST must be shorter than REPORT_SIZE");
 static_assert(sizeof(USBDataINShape) <= REPORT_SIZE, "Data going IN to HOST must be shorter than REPORT_SIZE");
